48588b02656520a016ca41f8ffbb1dc42a3ada75
Joe Karanka <joekaranka@Joes-iMac.local>
2020-03-01 15:41:56 +0200

    extra merge issues

--- src/app/user-list/user-routes/users-routing.module.ts
+++ src/app/user-list/user-routes/users-routing.module.ts
@@ -6,7 +6,7 @@ import { UserDetailsComponent } from "../user-details/user-details.component";
 
 const routes: Routes = [
   { path: "", component: UsersComponent },
-  { path: "/users/:id", component: UserDetailsComponent }
+  { path: "/:id", component: UserDetailsComponent }
 ];
 
 @NgModule({
